4048037360b90d1a069bf4a6a3ef2fa62d5c948d
Gabriel Leonel <gabrielvista7@gmail.com>
2025-09-23 14:24:36 -0300

    Started parser implementation

--- src/parser.rs
+++ src/parser.rs
@@ -2,10 +2,17 @@ use std::fs;
 use quick_xml::Reader;
 use quick_xml::events::Event;
 
-
+mod logger;
 
 fn main() {
+    logger::register_logger();
+    let file:Vec<u8>  = read_file_bytes("./data/Mod65.xml").unwrap();
 
+    let modelo: ModNfe = get_mod_nfe(&file);
+
+    println!("Modelo: {:?}", modelo);
+
+    /*
     let xml: String = fs::read_to_string("./data/Mod65.xml").unwrap();
     let conf: Config = Config::new_with_custom_values(leading_zero_as_string, xml_attr_prefix, xml_text_node_prop_name, empty_element_handling);
     let json = xml_string_to_json(xml.to_owned(), &conf);
@@ -15,6 +22,7 @@ fn main() {
     let conf: Config = Config::new_with_custom_values(true, "", "txt", NullValue::Null);
     let json = xml_string_to_json(xml.to_owned(), &conf);
     println!("{}", json.expect("Malformed XML").to_string());
+     */
 }
 
 
@@ -25,20 +33,40 @@ fn read_file_bytes(path: &str) -> std::io::Result<Vec<u8>> {
 }
 
 
-fn get_tipo_nfe(xml_bytes: &[u8]) -> TipoNFe {
-    let mut reader = Reader::From
-    let mut reader = Reader::from_reader(xml_bytes);
-    reader
-    let mut buf = Vec::new();
+fn get_mod_nfe(xml_bytes: &[u8]) -> ModNfe {
+    let mut reader: Reader<&[u8]> = Reader::from_reader(xml_bytes);
+    reader.config_mut().trim_text(true);
 
-    TipoNfe::Unknown
+
+
+
+    log::info!("Searching mod...");
+    loop {
+        match reader.read_event() {
+            Ok(Event::Start(e)) if e.name().as_ref() == b"mod" => {
+            log::info!("Found mod!!");
+            }
+
+            Ok(Event::Text(e)) => {
+                let txt: String = e.decode().unwrap().into_owned();
+                log::info!("Text: {}", txt);
+            }
+
+            Ok(Event::Eof) => break,
+            Err(_) => break,
+            _ => {}
+        }
+    }
+
+    return ModNfe::Desconhecido;
+}
+
+#[derive(Debug)]
+pub enum ModNfe {
+    Mod55,
+    Mod57,
+    Mod65,
+    Desconhecido
 }
 
 
-pub enum ModeloNFe {
-    EnviNFe,  // Envelope de envio (pode conter múltiplas NFe)
-    NFeProc,  // Documento processado (uma NFe)
-    NFCe,     // NFC-e (uma nota)
-    SAT,      // SAT (uma nota)
-    Desconhecido,
-}
